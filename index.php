f243266925a96f08840c3ee2604dc1d555d9d37d
dev-chris <dev.chris@icloud.com>
2015-06-13 02:25:18 -0400

    Small web
    Just dev of a website.

---
+++ index.php
@@ -0,0 +1,7 @@
+
+
+<link rel="stylesheet" type="text/css" href="default.css" media="screen, handheld" />
+<link rel="stylesheet" type="text/css" href="desktop.css" media="screen  and (min-width: 40.5em)" />
+<!--[if (lt IE 9)&(!IEMobile)]>
+<link rel="stylesheet" type="text/css" href="desktop.css" />
+<![endif]-->
