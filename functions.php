742572c725d85bf277c74180d4f50f783b6ec121
dev-chris <dev.chris@icloud.com>
2015-06-13 04:51:54 -0400

    more updates

--- functions.php
+++ functions.php
@@ -2,15 +2,17 @@
 function includeHeader () {
 	echo '<meta charset="UTF-8">
 	<!--[if lt IE 9]>
-	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]--> 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="reset.css"/>
 	<link rel="stylesheet" type="text/css" href="default.css" media="screen, handheld" />
 	<link rel="stylesheet" type="text/css" href="default.css" media="screen  and (min-width: 40.5em)" />
 	<!--[if (lt IE 9)&(!IEMobile)]>
-	<link rel="stylesheet" type="text/css" href="desktop.css" />
-	
-	<![endif]-->';
+		<link rel="stylesheet" type="text/css" href="default.css" />
+	<![endif]-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	';
 } 
 
 ?>
--- index.php
+++ index.php
@@ -9,11 +9,16 @@
 
 </head>
 <body>
-<div id="first">
 	
 <div id="nav">
-	<span>Logo</span><span>We can help</span><span>Team</span><span>Work</span><span>Contact us</span>
+	<img src="images/lbc_black_small.png" class="small_logo">
+	<div class="items">
+		<span><a href="#second">We can help</a></span><span>Team</span><span>Work</span><span>Contact us</span>
+	</div>
 </div>
+<div id="first">
+	
+
 	
 
 <h1>DESIGNED<br />
@@ -21,10 +26,45 @@ TO BE<br />
 A FORCE<br />
 FOR CHANGE</h1>
 
-<p class="white first"> You have the vision and conviction to make a difference, but you 
+<p class="first">You have the vision and conviction to make a difference, but you 
 need a partner to help get the word out? Perfect, believe in the beard.<p>
 
+</div>
 
+<div id="second">
+	
+
+	
+
+<h1>WE CAN HELP</h1>
+
+<p class="second">Longbeard Creative is intent on stoking a design revolution in the nonprofit sector by building front­-end media that more effectively engages, impassions, and empowers its users to be the force for change our world needs.<p>
+<div style="width: 100%">
+<img src="images/icons.png" class="icons1">
+<div class="clear"></div>
+<table>
+	<tr>
+		<td><p>WEBSITE & UX DESIGN</p>
+
+You don’t get a second chance at a first impression. Putting the user experience first is always a full-proof strategy. Websites are statements and we will help you make a bold one.
+		</td>
+		<td>
+			
+			<p>PRINT</p>
+
+Making a splash in print media remains a powerful way to engage your client base. We will design you a poster, t-shirt, or social media covers that will make waves and turn heads.
+		</td>
+		<td>
+			<p>ONLINE VIDEO</p>
+
+Over 75% of internet traffic is online video. Investing in engaging video production is essential to your organization’s communication strategy. Let our gurus give you a hand.
+		</td>
+	</tr>
+</table>
+</div>
+<div class="videoWrapper">
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/lJ3aD3EPtHM?vq=hd720&showinfo=0&controls=0" frameborder="0" allowfullscreen></iframe>
+</div>
 	
 	
 </div>
@@ -46,7 +86,7 @@ $('a[href*=#]:not([href=#])').click(function() {
            if (target.length) {
              $('html,body').animate({
                  scrollTop: target.offset().top
-            }, 1000);
+            }, 2000);
             return false;
         }
     }
